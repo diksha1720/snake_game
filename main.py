dd1346ae943b511b393805a303af56e53f454e6d
diksha1720 <dikshabharti39@gmail.com>
2021-12-26 19:32:57 +0530

    added images

--- main.py
+++ main.py
@@ -35,15 +35,18 @@ while flag:
         snake.extend()
 
     #Detect collision with wall.
-    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        flag = False
+    if snake.head.xcor() > 280 or snake.head.xcor()<-280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.game_over()
+        snake.restart()
+
+
 
     #Detect colission with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            flag = False
             scoreboard.game_over()
+            snake.restart()
+
 
 
 
